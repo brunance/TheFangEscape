eb82e48b6cb5bfe90bfeaee6183f28026b23ee25
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-11-13 11:46:01 -0400

    change to SwiftUI

---
+++ TheFangScape/TheFangScape/TheFangScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFangScapeApp.swift
+//  TheFangScape
+//
+//  Created by Victor Vasconcelos on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TheFangScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
