93d48729b9b23dcc0662ba127e0fb83136f03bed
Luciano Uchoa <lucianou54@gmail.com>
2023-11-21 11:48:47 -0400

    feat: DeathComponent

--- TheFangScape/TheFangScape/Components/ShootComponent.swift
+++ TheFangScape/TheFangScape/Components/ShootComponent.swift
@@ -10,12 +10,13 @@ import GameplayKit
 
 class ShootComponent: GKComponent {
     
+    weak var entityManager: SKEntityManager?
+    
     private var shootTimerCounter: TimeInterval = 0.0
     private let shootInterval: TimeInterval = 0.5
     
     var bulletVelocity: CGFloat
     var bulletDirection: PlayerDirection
-    weak var entityManager: SKEntityManager?
     var currentBullet: BulletEntity?
     
     public init(bulletVelocity: CGFloat, entityManager: SKEntityManager, bulletDirection: PlayerDirection) {
@@ -41,7 +42,7 @@ class ShootComponent: GKComponent {
         shootTimerCounter += seconds
         
         if shootTimerCounter >= shootInterval {
-            guard let currentBullet = currentBullet else {
+            guard let _ = currentBullet else {
                 shoot()
                 return
             }
@@ -50,9 +51,29 @@ class ShootComponent: GKComponent {
         
         if let bullet = currentBullet {
             if bullet.component(ofType: PhysicsComponent.self)?.touchedOnWall(direction: bulletDirection) == true {
-                entityManager?.remove(entity: bullet)
-                currentBullet = nil
+                removeBullet(bullet)
             }
+        }
+    }
+    
+    private func removeBullet(_ bullet: BulletEntity) {
+        
+        bullet.removeComponent(ofType: MovementComponent.self)
+        bullet.component(ofType: PhysicsComponent.self)?.body.affectedByGravity = true
+        bullet.component(ofType: PhysicsComponent.self)?.body.applyImpulse(CGVector(dx: -self.bulletDirection.rawValue * 0.1, dy: 0))
+        
+        let appearAction = SKAction.scale(to: 1.5, duration: 0.2)
+        let disappearAction = SKAction.scale(to: 0, duration: 0.2)
+        let removeAction = SKAction.removeFromParent()
+        
+        let visualEffectNode = SKSpriteNode(color: .white, size: CGSize(width: 10, height: 10))
+        
+        bullet.component(ofType: GKSKNodeComponent.self)?.node.addChild(visualEffectNode)
+        
+        let sequence = SKAction.sequence([appearAction, disappearAction, removeAction])
+        visualEffectNode.run(sequence) {
+            self.entityManager?.remove(entity: bullet)
+            self.currentBullet = nil
         }
     }
 }
